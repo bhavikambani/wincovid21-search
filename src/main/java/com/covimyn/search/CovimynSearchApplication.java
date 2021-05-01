333230de67a3e3d3128b85bcdfe99cc17b081c87
Bhavik Ambani <bhavikkumar.ambani@myntra.com>
2021-05-01 13:34:38 +0530

    Compilation error.

--- src/main/java/com/covimyn/search/CovimynSearchApplication.java
+++ src/main/java/com/covimyn/search/CovimynSearchApplication.java
@@ -1,26 +1,15 @@
 package com.covimyn.search;
 
-import com.covimyn.search.dao.ResourceDao;
-import com.covimyn.search.dao.ResourceDaoImpl;
-import com.covimyn.search.model.ResourceModel;
-import com.covimyn.search.utility.EsQueryBuilder;
-import com.covimyn.search.utility.HttpHelper;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CovimynSearchApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		//SpringApplication.run(CovimynSearchApplication.class, args);
+        SpringApplication.run(CovimynSearchApplication.class, args);
 
-
-	}
-
-
-
-
+    }
 
 }
